68c1401c2fd8428df1765f4426377380ac9f8bea
Dmytro Dovganiuk <xdovgan@nymfe44.fi.muni.cz>
2018-03-06 17:24:19 +0100

    Mission Assignment added

---
+++ src/Galerie-Backend/src/main/java/cz/muni/fi/pv168/agents/backend/MissionAssignment.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pv168.agents.backend;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author xdovgan
+ */
+public class MissionAssignment {
+    private Long id;
+    private Mission mission;
+    private List <Agent> agents;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Mission getMission() {
+        return mission;
+    }
+
+    public void setMission(Mission mission) {
+        this.mission = mission;
+    }
+
+    public List<Agent> getAgents() {
+        return agents;
+    }
+
+    public void setAgents(List<Agent> agents) {
+        this.agents = agents;
+    }
+
+    @Override
+    public String toString() {
+        return "MissionAssignment{" + "id=" + id + ", mission=" + mission + ", agents=" + agents + '}';
+    }
+
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MissionAssignment other = (MissionAssignment) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+    
+    
+}
