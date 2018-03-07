e585774abe96e930f466da04fafd6ce7818d75d1
Dima <Dima@147.251.212.26>
2018-03-07 21:13:24 +0100

    new interface and data

--- src/Galerie-Backend/src/main/java/cz/muni/fi/pv168/agents/backend/Agent.java
+++ src/Galerie-Backend/src/main/java/cz/muni/fi/pv168/agents/backend/Agent.java
@@ -1,86 +1,21 @@
 package cz.muni.fi.pv168.agents.backend;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 
 public class Agent {
 
-    private Long id;
+    private final Long id;
+    private final LocalDate born;
+    private String level;
     private String name;
-    private LocalDate lastEdit;
-    private byte[] data;
 
-    public void setId(Long id) {
+    public Agent(Long id, LocalDate born, String level, String name) {
         this.id = id;
-    }
-
-    public void setName(String name) {
+        this.born = born;
+        this.level = level;
         this.name = name;
     }
-
-    public void setLastEdit(LocalDate lastEdit) {
-        this.lastEdit = lastEdit;
-    }
-
-    public void setData(byte[] data) {
-        this.data = data;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDate getLastEdit() {
-        return lastEdit;
-    }
-
-    public byte[] getData() {
-        return data;
-    }
-
-    @Override
-    public String toString() {
-        return "Photo{"
-                + "id=" + id
-                + ", name=" + name
-                + ", lastEdit=" + lastEdit
-                + ", data=" + data
-                + '}';
-    }
-
-    /**
-     * Returns true if obj represents the same grave. Two objects are considered
-     * to represent the same grave when both are instances of {@link Agent}
-     * class, both have assigned some id and this id is the same.
-     *
-     *
-     * @param obj the reference object with which to compare.
-     * @return true if obj represents the same grave.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Agent other = (Agent) obj;
-        if (obj != this && this.id == null) {
-            return false;
-        }
-        return Objects.equals(this.id, other.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(this.id);
-    }
-
+    
 
 }
