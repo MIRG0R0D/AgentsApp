0f8d8060ea4db0fb5859c7d4818bc8c6983ff8d0
Dima <Dima@147.251.212.26>
2018-04-02 20:27:57 +0200

    some agent demo

--- src/Galerie-Backend/src/main/java/cz/muni/fi/pv168/agents/backend/SecretDemo.java
+++ src/Galerie-Backend/src/main/java/cz/muni/fi/pv168/agents/backend/SecretDemo.java
@@ -1,16 +1,18 @@
 package cz.muni.fi.pv168.agents.backend;
 
 
+import java.time.LocalDate;
+import java.time.Month;
 import org.apache.derby.jdbc.ClientDataSource;
 
 import javax.sql.DataSource;
 
 public class SecretDemo {
 
-    private DataSource ds;
-    private SecretManager secretManager;
-    private MissionManager missionManager;
-    private AgentManager agentManager;
+    private static DataSource ds;
+    private static SecretManager secretManager;
+    private static MissionManager missionManager;
+    private static AgentManager agentManager;
 
     public static void main(String[] args) {
         ClientDataSource ds = new ClientDataSource();
@@ -19,6 +21,37 @@ public class SecretDemo {
         ds.setDatabaseName("user-test");
         SecretDemo demo = new SecretDemo(ds);
         demo.show();
+        agent_demo();
+        
+    }
+    
+    private static void agent_demo(){
+        System.out.println("Agent demo \n");
+        System.out.println("adding agents");
+        Long jamesID=null, tomID = null, chuckID=null;
+        try{
+            jamesID=agentManager.create(new Agent(Long.MIN_VALUE, LocalDate.now(), "00", "James Bond"));
+            tomID=agentManager.create(new Agent(Long.MIN_VALUE, LocalDate.now(), "15", "Tom Ford"));
+            chuckID=agentManager.create(new Agent(Long.MIN_VALUE, LocalDate.now(), "15", "Chuck Norris"));
+        }catch (Exception e){ e.printStackTrace(); }
+        
+        System.out.println("Finding  agents by id");
+        try{
+            System.out.println(agentManager.findAgentById(tomID).toString());
+        }catch (Exception e){ e.printStackTrace(); }
+        
+        System.out.println("Updating");
+        try{
+            System.out.println(agentManager.findAgentById(jamesID).toString());
+            agentManager.update(jamesID, new Agent(Long.MIN_VALUE, LocalDate.now(), "00", "Bond, James Bond"));
+            System.out.println(agentManager.findAgentById(jamesID).toString());
+        }catch (Exception e){ e.printStackTrace(); }
+        
+        System.out.println("Find all agents");
+        try{
+            for (Agent ag : agentManager.findAllAgents())
+                System.out.println(ag.toString());
+        }catch (Exception e){ e.printStackTrace(); }
     }
 
     private SecretDemo(DataSource ds) {
